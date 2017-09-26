38a390648214f939f72be9d1a995161ffbf0b350
paul <paul604@users.noreply.github.com>
2017-09-26 16:28:45 +0200

    restructure

--- src/bots/paul.js
+++ src/bots/paul.js
@@ -1,13 +1,99 @@
 function iaGenerator(mapSize) {
-        var teleport = false;
-        var out = false;
-        var outx;
-        var outy;
-        var decalx=42;
-        var decaly=42;
-        var map;
-        var mouvChoice;
     return {
+        teleport: false,
+        out : false,
+        outx: null,
+        outy: null,
+        decalx:42,
+        decaly: 42,
+        map: null,
+        mouvChoice: null,
+
+
+        //---------------------------------------------------------------------------
+
+        saveWall: function saveWall(walls, mapSize) {
+            this.map = new Array(mapSize);
+
+            //init map
+            for (var x = 0; x < mapSize; x++) {
+              this.map[x]=new Array(mapSize);
+              for (var y = 0; y < mapSize; y++) {
+                  this.map[x][y]=0
+              }
+            }
+
+            // init wall
+            for (var wall of walls) {
+            this.map[wall.x][wall.y]=1;
+            }
+        },
+
+        initMouvChoice: function initMouvChoice() {
+            this.mouvChoice = new Array();
+            this.mouvChoice[0]={x:0, y:-1};
+            this.mouvChoice[1] ={x:1, y:-1};
+            this.mouvChoice[2] ={x:1, y:0};
+            this.mouvChoice[3] ={x:1, y:1};
+            this.mouvChoice[4] ={x:0, y:1};
+            this.mouvChoice[5] ={x:-1, y:1};
+            this.mouvChoice[6] ={x:-1, y:0};
+            this.mouvChoice[7] ={x:-1, y:-1};
+        },
+
+        getMouvChoice: function getMouvChoice(x, y) {
+            this.mouvChoice.forEach(function (element, index) {
+                if(element.x === x && element.y === y){
+                    return index;
+                }
+            });
+            return NaN;
+        },
+
+        choixMouve: function choixMouve(choice, count){
+          // if(x === 0 && y ===-1){//haut
+          //   return  {x:1 ,y:y};
+          // }else if(x === 1 && y === -1){//hd
+          //   return  {x:x ,y:0};
+          // }else if(x === 1 && y === 0){//d
+          //   return  {x:x ,y:1};
+          // }else if(x === 1 && y === 1){//bd
+          //   return  {x:0 ,y:y};
+          // }else if(x === 0 && y === 1){//b
+          //   return  {x:-1 ,y:y};
+          // }else if(y === 1){//bg
+          //   return  {x:x ,y:0};
+          // }else if(y === 0){//g
+          //   return  {x:x ,y:-1};
+          // }else if(y === -1){//hg
+          //   return  {x:0 ,y:-1};
+          // }else {
+          //   return {x:0 ,y:0};
+          // }
+        },
+
+        testMove: function testMove(mapSize, position, moveX, moveY) {
+            var testx=position.x+moveX;
+            var testy=position.y+moveY;
+            if (testx<0 || testx>=mapSize || testy<0 || testy>=mapSize || this.map[testx][testy] === 1) {
+            return false;
+            }
+            return true;
+        },
+
+        move: function move(mapSize, position, moveX, moveY) {
+            if(!this.testMove(mapSize, position, moveX, moveY)){
+            //   var tabMove = choixMouve(moveX, moveY);
+              var tabMove = this.choixMouve(this.getMouvChoice(moveX, moveY), 1);
+              moveX=tabMove.x;
+              moveY=tabMove.y;
+              return this.move(mapSize, position, moveX, moveY)
+            }
+            return {x:moveX, y:moveY};
+        },
+
+
+        //--------------------------------------------------------------
 
 
         /**
@@ -26,9 +112,9 @@ function iaGenerator(mapSize) {
          * @return {void}
          */
         onFriendWins: function onFriendWins(exit) {
-            out=true;
-            outx=exit.x;
-            outy=exit.y;
+            this.out=true;
+            this.outx=exit.x;
+            this.outy=exit.y;
             console.log(exit);
         },
 
@@ -43,7 +129,7 @@ function iaGenerator(mapSize) {
             //1 je suis trop à gauche
             //-1 je suis trop à droite
             //0 je suis en face de la sortie
-            decalx=hPosition;
+            this.decalx=hPosition;
             console.log("paul x: "+hPosition);
         },
 
@@ -58,7 +144,7 @@ function iaGenerator(mapSize) {
             //1 je suis trop bas
             //-1 je suis trop haut
             //0 je suis en face de la sortie
-            decaly=vPosition;
+            this.decaly=vPosition;
             console.log("paul y: "+vPosition);
         },
 
@@ -74,28 +160,28 @@ function iaGenerator(mapSize) {
         action: function action(position, round, walls) {
           console.log("action");
           if (round === 0) {
-            map = saveWall(walls, mapSize);
-            mouvChoice = initMouvChoice();
+            this.saveWall(walls, mapSize);
+            this.initMouvChoice();
           }
           var x=0;
           var y=0;
           var choix = "";
           var ask;
 
-          if(out && !teleport){
+          if(this.out && !this.teleport){
             choix = "teleport";
-          }else if(out && teleport){
+          }else if(this.out && this.teleport){
             choix = "move";
-          }else if(decalx===42){
+          }else if(this.decalx===42){
             choix = "ask";
             ask="x";
-          }else if(decaly === 42){
+          }else if(this.decaly === 42){
             choix = "ask";
             ask="y";
           }else {
             choix = "move";
-            x=decalx;
-            y=decaly;
+            x=this.decalx;
+            y=this.decaly;
           }
 
 
@@ -105,15 +191,15 @@ function iaGenerator(mapSize) {
 
           switch (choix){
             case "move":
-              console.log(move);
-              if(out && teleport){
-                x=outx-position.x;
-                y=outy-position.y;
+              console.log(this.move);
+              if(this.out && this.teleport){
+                x=this.outx-position.x;
+                y=this.outy-position.y;
               }else{
-                resultMove = move(mapSize, position, map, x, y);
+                resultMove = this.move(mapSize, position, x, y);
                 if(resultMove.x !== 0 && resultMove.y !== 0){
-                  decalx=42;
-                  decaly=42;
+                  this.decalx=42;
+                  this.decaly=42;
                 }
                 x=resultMove.x;
                 y=resultMove.y;
@@ -133,12 +219,12 @@ function iaGenerator(mapSize) {
               };
               break;
             case "teleport":
-              teleport=true;
+              this.teleport=true;
               var positionOut ={
-                x: outx,
-                y: outy
+                x: this.outx,
+                y: this.outy
               };
-              resultMove = move(mapSize, positionOut, map, 1, 0);
+              resultMove = this.move(mapSize, positionOut, 1, 0);
               x=positionOut.x+resultMove.x;
               y=positionOut.y+resultMove.y;
               action = {
@@ -151,90 +237,8 @@ function iaGenerator(mapSize) {
           }
             return action;
         }
+
     };
-}
-
-//---------------------------------------------------------------------------
-
-function saveWall(walls, mapSize) {
-  var localMap = new Array(mapSize);
-
-  //init map
-  for (var x = 0; x < mapSize; x++) {
-      localMap[x]=new Array(mapSize);
-      for (var y = 0; y < mapSize; y++) {
-          localMap[x][y]=0
-      }
-  }
-
-    // init wall
-  for (var wall of walls) {
-    localMap[wall.x][wall.y]=1;
-  }
-  return localMap;
-}
-
-function initMouvChoice(mouvChoice) {
-    mouvChoice = new Map();
-    mouvChoice.set(0,{x:0, y:-1});
-    mouvChoice.set(1,{x:1, y:-1});
-    mouvChoice.set(2,{x:1, y:0});
-    mouvChoice.set(3,{x:1, y:1});
-    mouvChoice.set(4,{x:0, y:1});
-    mouvChoice.set(5,{x:-1, y:1});
-    mouvChoice.set(6,{x:-1, y:0});
-    mouvChoice.set(7,{x:-1, y:-1});
-}
-
-function getMouvChoice(x, y) {
-    for (var [key, valeur] of mouvChoice) {
-        if(valeur.x === x && valeur.y === y){
-            return key;
-        }
-    }
-    return NaN;
-}
-
-function move(mapSize, position, map, moveX, moveY) {
-  if(!testMove(mapSize, position, map, moveX, moveY)){
-    //   var tabMove = choixMouve(moveX, moveY);
-      var tabMove = choixMouve(getMouvChoice(moveX, moveY), 1);
-      moveX=tabMove.x;
-      moveY=tabMove.y;
-      return move(mapSize, position, map, moveX, moveY)
-  }
-  return {x:moveX, y:moveY};
-}
-
-function testMove(mapSize, position, map, moveX, moveY) {
-  var testx=position.x+moveX;
-  var testy=position.y+moveY;
-  if (testx<0 || testx>=mapSize || testy<0 || testy>=mapSize || map[testx][testy] === 1) {
-    return false;
-  }
-  return true;
-}
-
-function choixMouve(choice, count){
-  // if(x === 0 && y ===-1){//haut
-  //   return  {x:1 ,y:y};
-  // }else if(x === 1 && y === -1){//hd
-  //   return  {x:x ,y:0};
-  // }else if(x === 1 && y === 0){//d
-  //   return  {x:x ,y:1};
-  // }else if(x === 1 && y === 1){//bd
-  //   return  {x:0 ,y:y};
-  // }else if(x === 0 && y === 1){//b
-  //   return  {x:-1 ,y:y};
-  // }else if(y === 1){//bg
-  //   return  {x:x ,y:0};
-  // }else if(y === 0){//g
-  //   return  {x:x ,y:-1};
-  // }else if(y === -1){//hg
-  //   return  {x:0 ,y:-1};
-  // }else {
-  //   return {x:0 ,y:0};
-  // }
 }
 
 module.exports = iaGenerator;
